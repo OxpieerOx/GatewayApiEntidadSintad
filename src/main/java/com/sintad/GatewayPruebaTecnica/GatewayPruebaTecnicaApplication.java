85b1d0c8bf62ea5f9121fd892fd7cdad45d33e71
Pieer <prodas@sigcomt.com>
2022-10-27 22:36:19 -0500

    first commit

---
+++ src/main/java/com/sintad/GatewayPruebaTecnica/GatewayPruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.sintad.GatewayPruebaTecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPruebaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sintad/GatewayPruebaTecnica/GatewayPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sintad.GatewayPruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
